8ed3bd16ba4efb903a01b68e0d0e3ad63e93bcdc
Lucas <784354821@qq.com>
2026-01-16 15:48:08 +0800

    feat(数据库): 添加TypeORM模块和MySQL数据库配置
    添加TypeORM模块支持并配置MySQL数据库连接
    在main.ts中添加应用运行地址的日志输出
    更新package.json添加相关依赖

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // 数据库创建语句：CREATE DATABASE meeting_room_booking_system DEFAULT CHARACTER SET utf8mb4;
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 数据库类型
+      host: 'localhost', // 数据库地址
+      port: 3306, // 数据库端口
+      username: 'root', // 用户名
+      password: '123456', // 密码
+      database: 'meeting_room_booking_system', // 数据库名称
+      synchronize: true, // 是否自动同步实体到数据库表，生产环境建议关闭
+      logging: true, // 是否打印日志
+      entities: [], // 实体列表
+      poolSize: 10, // 连接池大小
+      connectorPackage: 'mysql2', // 指定连接器包
+      extra: {
+        authPlugin: 'sha256_password', // 额外配置，这里指定了认证插件
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
